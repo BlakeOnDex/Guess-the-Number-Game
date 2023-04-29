89c30e04645c689e13624c7da8f99a815186b5a6
BlakeOnDex <108043930+BlakeOnDex@users.noreply.github.com>
2023-04-28 22:56:02 -0400

    Update Guess the Number Game.bat

--- Guess the Number Game.bat
+++ Guess the Number Game.bat
@@ -1,37 +1,35 @@
 @echo off
 title Guess the Number Game
 
-set/a num=%random% %%100
+set /a num=%random% %% 100
 
-echo
-echo Welcome to the Guess the Number
-Game!
-echo I am thinking of a number between 1 and 100
+echo.
+echo Welcome to the Guess the Number Game!
+echo I am thinking of a number between 1 and 100.
 echo Can you guess what it is?
 echo.
 
-set/a guesses=0
+set /a guesses=0
 
 :guess
-set/p input="Enter your guess:"
+set /p input="Enter your guess: "
 
-set/a guesses+=1
+set /a guesses+=1
 
-if %input%==%num% (
-  echo
-  echo Congratulations! you guessed the 
-number in %guesses% guesses.
-echo
-goto end
-)else if %input% gtr %num% (
- echo
- echo your guess was too high. Try again.
- echo
- goto guess
-)else(
+if %input% equ %num% (
   echo.
-  echo your guess was too low. Try again.
-  echo
+  echo Congratulations! You guessed the number in %guesses% guesses.
+  echo.
+  goto end
+) else if %input% gtr %num% (
+  echo.
+  echo Your guess was too high. Try again.
+  echo.
+  goto guess
+) else (
+  echo.
+  echo Your guess was too low. Try again.
+  echo.
   goto guess
 )
 
